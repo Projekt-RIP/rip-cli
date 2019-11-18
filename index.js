5b12255d2d75a7f55af3cd997ae735817e1a18db
tjarbo <16938041+tjarbo@users.noreply.github.com>
2019-11-18 10:47:36 +0000

    :building_construction: Changed input and output files

--- index.js
+++ index.js
@@ -3,35 +3,13 @@
  * 15.11.2019
  */
 
-const welcomeBanner = require("./lib/output/welcome-banner")
-const help = require("./lib/output/help")
-var argv = require('minimist')(process.argv.slice(2))
-
+const input = require("./lib/input")
+const processing = require("./lib/processing")
+const output = require("./lib/output")
+const argv = require('minimist')(process.argv.slice(2))
 
 function run() {
-    switch (true) {
-        case ((argv.h != undefined) || (argv.help != undefined)):
-            help.printHelp()
-            break;
-            
-        case (argv._.includes("parameter")):
-            welcomeBanner.show()
-            const data = require("./lib/input/paramter").convert(argv)
-            
-            break;
-
-        case (argv._.includes("file") || argv._.includes("interactive") || argv._.includes("passwordmanager")):
-            welcomeBanner.show()
-            help.printHelp()
-            require("terminal-banner").terminalBanner("Sorry, but this is not implemented yet!")
-            break;
-        
-        default:
-            console.log("Invalid arguments: ", argv);
-            help.printHelp()
-            process.exit(0)
-            break;
-    }
+    const data = input.data(argv)
 }
 
 run()
